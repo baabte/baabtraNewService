9c697226c3c7cb5cdb66f00e328f0687ee8b51a0
Lijin <lijin@baabte.com>
2015-07-10 12:32:53 +0530

    local changes

--- Djproject/wsgi.py
+++ Djproject/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Djproject.settings")
+# import monitor
+# monitor.start(interval=1.0)
+# monitor.track(os.path.join(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
